0643307453b8de6652c61f881b4d8594187154c3
surya <suryar.aids2021@citchennai.net>
2025-02-07 18:57:39 +0530

    Update hello.bat

--- hello.bat
+++ hello.bat
@@ -1,12 +1,4 @@
 @echo off
 echo Hello, World! from external bat file
 
-:: Create directory "test" 
-if not exist test mkdir test
-
-:: Change directory to "test"
-cd test
-
-:: Create file "file1"
-type nul > file1
 
